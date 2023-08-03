7cc85f71e0821ec22f49fb6ea430354501d7f507
Akrtripathi1 <akashtripathi2002@gmail.com>
2023-08-03 06:22:12 +0530

    ..

---
+++ Assignment3/q4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int lcm_three (int a,int b,int c )
+{
+    
+}
+int main()
+{
+    int s,d,e,j;
+    cout<<"enter three number you want to find lcm of :";
+    cin>>s>>d>>e;
+    j=lcm_three(s,d,e);
+    cout<<"L.C.M. is "<<j;
+}
